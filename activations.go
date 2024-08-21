c7d6f757c981b6ed95b82ef4d15badeae479ae67
Simon <simpan3497@gmail.com>
2024-08-21 14:02:20 +0200

    gjorde färdigt nätverket helt och komplett

--- activations.go
+++ activations.go
@@ -1,4 +1,20 @@
 package main
 
 type Activation interface {
+	call(float64) float64
+	ddx(float64) float64
+}
+
+type relU struct{}
+
+func (shelf *relU) call(val float64) float64 {
+	return max(0, val)
+}
+
+func (shelf *relU) ddx(val float64) float64 {
+	if val > 0 {
+		return 1
+	} else {
+		return 0
+	}
 }
