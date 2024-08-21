b69ba6d517562c1dfcb33fb04cc887c37b32ae5e
Gurad3 <matockanathan@gmail.com>
2024-08-21 13:52:05 +0200

    3d_to_flatt

--- mim.go
+++ mim.go
@@ -32,3 +32,24 @@ func (shelf *MiM) requst_3d(x int32, y int32, z int32) {
 		shelf.data_3d = arr3D
 	}
 }
+
+func (shelf *MiM) requst_flatt(x int32, y int32, z int32) {
+	switch shelf.data_type {
+	case ThreeD:
+
+		new3d := make([][][]float64, x)
+		index := 0
+		for i := int32(0); i < x; i++ {
+			new3d[i] = make([][]float64, y)
+			for j := int32(0); j < y; j++ {
+				new3d[i][j] = make([]float64, z)
+				for k := int32(0); k < z; k++ {
+					new3d[i][j][k] = shelf.data_flat[index]
+					index++
+				}
+			}
+		}
+
+		shelf.data_3d = new3d
+	}
+}
