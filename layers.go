4e6150bb9b5cda0a721a9d1d1f7ec25e26dcb6ea
Gurad3 <matockanathan@gmail.com>
2024-08-22 08:01:38 +0200

    Tänkte börja

--- layers.go
+++ layers.go
@@ -4,8 +4,14 @@ type Layers interface {
 	forward()
 }
 
+type BaseLayer struct {
+	act_func func(float64) float64
+}
+
 type DenseLayer struct {
+	base_layer BaseLayer
 }
 
 type CNLayer struct {
+	base_layer BaseLayer
 }
