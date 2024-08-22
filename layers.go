e3ddbe6ae1d50a6ec246c326a877f325f4b53277
Gurad3 <matockanathan@gmail.com>
2024-08-22 09:27:16 +0200

    h

--- layers.go
+++ layers.go
@@ -4,16 +4,16 @@ type Layers interface {
 	forward()
 }
 
-type BaseLayer struct {
-	act_interface Activation
-}
-
 type DenseLayer struct {
+	act_interface Activation
+
 	weights [][]float64
 	biases  []float64
 }
 
 type CNLayer struct {
+	act_interface Activation
+
 	weights [][][]float64
 	biases  [][]float64
 }
