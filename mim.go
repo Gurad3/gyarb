21534e7c61a544fba664029f9626ee3184e74f94
Gurad3 <matockanathan@gmail.com>
2024-08-24 19:04:56 +0200

    why?

--- mim.go
+++ mim.go
@@ -6,6 +6,7 @@ const (
 	ThreeD
 )
 
+/*
 type MiM struct {
 	data_flat []float64
 	data_2d   [][]float64
@@ -15,7 +16,78 @@ type MiM struct {
 	data_type_history []int32
 }
 
+
 func (shelf *MiM) request_3d(x int32, y int32, z int32) *MiM {
+	switch shelf.data_type {
+	case OneD:
+		arr3D := make([][][]float64, x)
+		index := 0
+		for i := int32(0); i < x; i++ {
+			arr3D[i] = make([][]float64, y)
+			for j := int32(0); j < y; j++ {
+				arr3D[i][j] = make([]float64, z)
+				for k := int32(0); k < z; k++ {
+					arr3D[i][j][k] = shelf.data_flat[index]
+					index++
+				}
+			}
+		}
+		shelf.data_3d = arr3D
+	}
+
+	return shelf
+}
+
+func (shelf *MiM) request_flat() *MiM {
+	switch shelf.data_type {
+	case ThreeD:
+
+		new_flat := make([]float64, len(shelf.data_3d)*len(shelf.data_3d[0])*len(shelf.data_3d[0][0]))
+
+		for i := 0; i < len(shelf.data_3d); i++ {
+			for j := 0; j < len(shelf.data_3d[0]); j++ {
+				new_flat = append(new_flat, shelf.data_3d[i][j]...)
+			}
+		}
+
+		shelf.data_flat = new_flat
+	}
+
+	return shelf
+}
+*/
+
+type MiM struct {
+	data_flat *[]float64
+	data_2d   *[][]float64
+	data_3d   *[][][]float64
+
+	data_type         int32
+	data_type_history []int32
+
+	layers_out_flat []*[]float64
+	layers_out_2d   []*[][]float64
+	layers_out_3d   []*[][][]float64
+
+	layersDimentions [][]int32
+}
+
+func (shelf *MiM) init_MiM(layersDimentions [][]int32) {
+	shelf.layersDimentions = layersDimentions
+
+	for layerID, layerDim := range layersDimentions {
+		switch len(layerDim) - 1 {
+		case OneD:
+			newAr := make([]float64, layerDim[0])
+			shelf.layers_out_flat[layerID] = &newAr
+		case ThreeD:
+
+		}
+	}
+
+}
+
+func (shelf *MiM) request_3d() *MiM {
 	switch shelf.data_type {
 	case OneD:
 		arr3D := make([][][]float64, x)
