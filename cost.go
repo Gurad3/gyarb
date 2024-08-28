cb0ec2f17236a7b427e0835fd402c5c1f484342c
Simon <simpan3497@gmail.com>
2024-08-28 09:10:49 +0200

    galet

--- cost.go
+++ cost.go
@@ -1,7 +1,14 @@
 package main
 
 type Cost interface {
-	call([]float64) float64
+	call([]float64, []float64) float64
 
 	ddx(float64) float64
+}
+
+type MeanSquare struct {
+}
+
+func (shelf *MeanSquare) call(actual_values []float64, target_values []float64) float64 {
+
 }
