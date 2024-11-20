c60f4a13b645c34456243abe55b7258f83f81b3b
Gurad3 <matockanathan@gmail.com>
2024-11-20 10:35:06 +0100

    Cleaning 1

--- layers.go
+++ layers.go
@@ -5,23 +5,23 @@ import (
 )
 
 type Layer interface {
-	forward(*MiM)                                      // Har
-	backprop(mim *MiM, prev_layer_act Activation)      // Har
+	forward(*MiM)                                 // Har
+	backprop(mim *MiM, prev_layer_act Activation) // Har
+	get_act_interface() Activation
+	apply_gradients(learn_rate float64, batch_size int, regularization float64, momentum float64)
+
 	init(layerID int, prev_layer_size []int)           // Har
 	init_new_weights(xavierRange float64, r rand.Rand) // Har
-	apply_gradients(learn_rate float64, batch_size int, regularization float64, momentum float64)
 
 	load_weights(flat_weights []float64)
 	load_biases(flat_biases []float64)
 	get_weights() []float64
 	get_biases() []float64
-
-	print_weights()
-	get_size() []int
-	get_name() string
 	get_init_vals() []int
 
-	get_act_interface() Activation
+	get_size() []int
+	get_name() string
 
+	print_weights()
 	debug_print()
 }
